c0ef638cd79141868395dc0ed80b0de1a08b6363
Chathura-Nilaksha <cnilakshami@gmail.com>
2024-03-17 22:31:55 +0530

    3.17

--- src/app/page/view-all-books/view-all-books.component.ts
+++ src/app/page/view-all-books/view-all-books.component.ts
@@ -1,10 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-all-books',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule, FormsModule],
   templateUrl: './view-all-books.component.html',
   styleUrl: './view-all-books.component.css'
 })
@@ -12,6 +15,7 @@ import { Component, OnInit } from '@angular/core';
 export class ViewAllBooksComponent implements OnInit{
   private http;
   public bookList:any = { };
+  public selectedBook: any;
 
   constructor(private httpClient:HttpClient){
     this.http = httpClient;
@@ -20,9 +24,44 @@ export class ViewAllBooksComponent implements OnInit{
   ngOnInit(): void {
     this.loadBooks();
   }
+
   loadBooks(){
     this.http.get('http://localhost:8080/book/get').subscribe((data)=> {
-      console.log(data);
-    })
+      this.bookList = data;
+      console.log(this.bookList);
+    });
   }
+  deleteBook(){
+    let api="http://localhost:8080/book/"+this.selectedBook;
+    this.http.delete(api,{responseType:'text'}).subscribe((response:string)=>{
+      console.log(response);
+      this.loadBooks();
+      Swal.fire({
+        title: "DELETED!",
+        text: `${this.selectedBook.title} book is deleted`,
+        icon: "success"
+      });
+      this.selectedBook=null;
+    });
+  }
+  setSelectedBook(book:any){
+    this.selectedBook=book;
+  }
+
+
+  saveBook(){
+    let postApi="http://localhost:8080/book/add";
+    this.http.post(postApi, this.selectedBook).subscribe((data)=>{
+      console.log("saved!");
+      this.loadBooks();
+      Swal.fire({
+        title: "UPDATED!",
+        text: `${this.selectedBook.title} book is updated`,
+        icon: "success"
+      });
+      this.selectedBook=null;
+    })
+  } 
+  
+  
 }
