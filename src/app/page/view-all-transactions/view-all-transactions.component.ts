b7c9a83f6b7fbd3806d1a3c80692c052956914c7
Chathura-Nilaksha <cnilakshami@gmail.com>
2024-03-24 20:13:56 +0530

    Building-View all transactions

--- src/app/page/view-all-transactions/view-all-transactions.component.ts
+++ src/app/page/view-all-transactions/view-all-transactions.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavComponent } from '../../common/nav/nav.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-all-transactions',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent, HttpClientModule, CommonModule],
   templateUrl: './view-all-transactions.component.html',
   styleUrl: './view-all-transactions.component.css'
 })
-export class ViewAllTransactionsComponent {
-  
+export class ViewAllTransactionsComponent implements OnInit{
+
+  public allTransactions:any=[ ];
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void {
+    this.loadTransactions();
+    
+  }
+  loadTransactions(){
+    this.http.get("http://localhost:8082/get-all").subscribe(data=>{
+      console.log(data);
+      this.allTransactions=data;
+    });
+  }
+
 }
