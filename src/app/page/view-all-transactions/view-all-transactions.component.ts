522000f17269f22fffe1c73e2b7829c9b90a080d
Chathura-Nilaksha <cnilakshami@gmail.com>
2024-03-24 17:53:42 +0530

    Made view-all-transactions component

---
+++ src/app/page/view-all-transactions/view-all-transactions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-transactions',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-all-transactions.component.html',
+  styleUrl: './view-all-transactions.component.css'
+})
+export class ViewAllTransactionsComponent {
+
+}
